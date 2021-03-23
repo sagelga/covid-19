616c6bd42bff5f476451f61a1621819add8ddf02
Kunanon Srisuntiroj <son9912@gmail.com>
2021-03-23 10:57:06 +0000

    Download dataset + read df
    - Fixes #3

--- src/core.py
+++ src/core.py
@@ -1,1 +1,10 @@
-pandas.read_csv('owid-covid-data.csv')
+import pandas as pd
+import requests
+
+# Import data from GH
+url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
+r = requests.get(url, allow_redirects=True)
+open('src/owid-covid-data.csv', 'wb').write(r.content)
+
+# Read data to Dataframe
+df = pd.read_csv('src/owid-covid-data.csv')
