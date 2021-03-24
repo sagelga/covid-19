467715917f053d0e411001dbe1e168d0ab18bd47
Kunanon Srisuntiroj <son9912@gmail.com>
2021-03-24 14:47:44 +0700

    Move read CSV part to another file

--- src/core.py
+++ src/core.py
@@ -8,23 +8,15 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-try:
-    # Read data to Dataframe
-    df = pd.read_csv('src/owid-covid-data.csv')
-except:
-    # Import data from GH
-    url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
-    r = requests.get(url, allow_redirects=True)
-    open('src/owid-covid-data.csv', 'wb').write(r.content)
-    # Read data to Dataframe
-    df = pd.read_csv('src/owid-covid-data.csv')
+# Importing data
+from df_import import df_import
 
 # Data selection
+df = df_import()
 df = df.dropna(subset=['continent'])
 
 # Create line chart
 fig = px.line(df, x="date", y="total_cases", title='Total Cases',
               color="continent", line_group="location", hover_name="location")
 
-# print(df)
 fig.show()
