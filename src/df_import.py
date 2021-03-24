dc7d0646ef1dd898166db3371cad609e02288c3a
Kunanon Srisuntiroj <son9912@gmail.com>
2021-03-24 15:23:49 +0700

    Remove src/ reference

--- src/df_import.py
+++ src/df_import.py
@@ -7,6 +7,6 @@ def df_import():
     # Import data from GH
     url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
     r = requests.get(url, allow_redirects=True)
-    open('src/owid-covid-data.csv', 'wb').write(r.content)
+    open('owid-covid-data.csv', 'wb').write(r.content)
 
     return pd.read_csv(url)
