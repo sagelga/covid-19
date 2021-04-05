a626a40c8589c486915fa2aac019787fff52f41a
Kunanon Srisuntiroj <son9912@gmail.com>
2021-04-06 01:02:48 +0700

    Add country count indicator

--- app.py
+++ app.py
@@ -12,47 +12,54 @@ import plotly.io as pio
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title= "COVID-19 Data Visualize"
+app.title = "COVID-19 Data Visualize"
 
 """
 Create Vaccinated-Total Population Line Chart
 """
+
+
 @app.callback(
     Output("vaccinate-timeline", "figure"),
     [Input("dropdown", "value")]
 )
 def vaccinate_timeline(countries):
-    # df = df_import()
-
-    # df = df.dropna(subset=['people_vaccinated'])
     mask = df.location.isin(countries)
-
-    # Calculate fields
-    df['ppl_vaccinated'] = df['people_vaccinated'] / df['population']
+    country_count = df.location.nunique()
+    country_selected = df[mask].location.nunique()
 
     fig = px.line(df[mask], x="date", y="ppl_vaccinated", title='Total Cases',
                   color="continent", hover_name="location")
-    fig.update_layout(title='Vaccinated population by Country',
-                      xaxis_title='Date', yaxis_title='Vaccinated (Percent)',
-                      font_family='Jetbrains Mono', title_font_family='Jetbrains Mono')
+    title = "Vaccinated population in {} of {} countries".format(
+        country_selected, country_count)
+    fig.update_layout(title=title,
+                      xaxis_title='Date', yaxis_title='Percent Vaccinated')
+    fig.update_traces(connectgaps=True, selector=dict(type='scatter'))
     return fig
 
 
 """
 Create Active Case Line Chart
 """
+
+
 @app.callback(
     Output("active-case", "figure"),
     [Input("dropdown", "value")]
 )
 def active_case(countries):
     mask = df.location.isin(countries)
+    country_count = df.location.nunique()
+    country_selected = df[mask].location.nunique()
 
     fig = px.line(df[mask], x="date", y="total_cases",
-                  color="continent", line_group="location", hover_name="location")
-    fig.update_layout(title='Total Case by Country',
-                      xaxis_title='Date', yaxis_title='Total Case',
-                      font_family='Jetbrains Mono', title_font_family='Jetbrains Mono')
+                  color="continent", hover_name="location")
+
+    title = "Total Case in {} of {} countries".format(
+        country_selected, country_count)
+    fig.update_layout(title=title,
+                      xaxis_title='Date', yaxis_title='Total Case')
+    fig.update_traces(connectgaps=True, selector=dict(type='scatter'))
     return fig
 
 
@@ -60,6 +67,8 @@ def active_case(countries):
 Function df_import() will imports the data from datasource
 and returns DataFrame that contains Pandas data.
 """
+
+
 def df_import():
 
     # Import data from GH
@@ -73,13 +82,20 @@ def df_import():
 # Data selection
 df = df_import()
 df = df.dropna(subset=['continent'])
+df = df.dropna(subset=['location'])
+df = df.dropna(subset=['date'])
+
+# Calculate fields
+df['ppl_vaccinated'] = (df['people_vaccinated'] / df['population'])*100
 
 # Selection bar
 all_country = df.location.unique()
 app.layout = html.Div([
     html.Div([
         html.H1('covid-vaccines'),
-
+        html.P('Datasource from https://github.com/owid/covid-19-data/'),
+    ]),
+    html.Div([
         dcc.Graph(id="active-case"),
         dcc.Graph(id="vaccinate-timeline"),
     ]),
@@ -89,11 +105,12 @@ app.layout = html.Div([
         dcc.Dropdown(
             id="dropdown",
             options=[{"label": x, "value": x}
-                    for x in all_country],
+                     for x in all_country],
             value=all_country,
             multi=True
         ),
     ]),
-], style={'font-family': 'Jetbrains Mono'})
+],)
 
-app.run_server(debug=True, use_reloader=False)# app.run_server(debug=True, host='0.0.0.0')
+# app.run_server(debug=True, host='0.0.0.0')
+app.run_server(debug=True, use_reloader=False)
