ea18b6746a82a12ecc655251b8db17a1afc8f9c1
Kunanon Srisuntiroj <son9912@gmail.com>
2021-04-06 23:29:47 +0700

    Officially migrates both graph

--- app.py
+++ app.py
@@ -5,32 +5,32 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 
+# App Initialize
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# app initialize
 app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}],
     external_stylesheets=external_stylesheets
 )
 server = app.server
+
 app.title = "COVID-19 Data Visualize"
 app.config["suppress_callback_exceptions"] = True
 
 # Data Import
 url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
 df = pd.read_csv(url)
-# r = requests.get(url, allow_redirects=True)
-# df = open('owid-covid-data.csv', 'wb').write(r.content)
 
 # Data Transform
 df.date = pd.to_datetime(df.date)
+df = df.sort_values(by=['date', 'location'])
 
 # Data selection
 df = df.dropna(subset=['continent'])
 df = df.dropna(subset=['location'])
 
 # Calculate fields
-df['ppl_vaccinated'] = 100*(df['people_vaccinated'] / df['population'])
+df['ppl_vaccinated'] = 100 * (df['people_vaccinated'] / df['population'])
 
 # Selection bar
 all_country = df.location.unique()
@@ -40,7 +40,7 @@ app.layout = html.Div([
         html.P('Datasource from https://github.com/owid/covid-19-data/'),
     ]),
     html.Div(children=[
-        # dcc.Graph(id="active-case"),
+        dcc.Graph(id="active-case"),
         dcc.Graph(id="vaccinate-timeline"),
     ]),
     html.Div(children=[
@@ -54,48 +54,59 @@ app.layout = html.Div([
             multi=True
         ),
     ]),
-],)
+])
 
 
-# Create Vaccinated-Total Population Line Chart
 @app.callback(
     Output("vaccinate-timeline", "figure"),
+    Output("active-case", "figure"),
     [Input("dropdown", "value")]
 )
-def vaccinate_timeline(countries):
+def update_graph(countries):
     mask = df.location.isin(countries)
     country_count = df.location.nunique()
     country_selected = df[mask].location.nunique()
 
-    fig = px.line(df[mask], x="date", y="ppl_vaccinated", title='Total Cases',
-                  color="continent", hover_name="location")
+    # Create Vaccinated-Total Population Line Chart
+    vcn_fig = px.line(
+        df[mask],
+        x="date",
+        y="ppl_vaccinated",
+        title='Total Cases',
+        color="location",
+        hover_name="location",
+        hover_data=['date', 'continent', 'people_vaccinated', 'population', 'ppl_vaccinated']
+    )
     title = "Vaccinated population in {} of {} countries".format(
         country_selected, country_count)
-    fig.update_layout(title=title,
-                      xaxis_title='Date', yaxis_title='Percent Vaccinated')
-    fig.update_traces(connectgaps=True, selector=dict(type='scatter'))
-    return fig
+    vcn_fig.update_layout(
+        title=title,
+        xaxis_title='Date',
+        yaxis_title='Percent Vaccinated'
+    )
+    vcn_fig.update_traces(connectgaps=True)
 
+    # Active Case Line Chart
+    atv_fig = px.line(
+        df[mask],
+        x="date",
+        y="total_cases",
+        color="location",
+        hover_name="location",
+        hover_data=['date', 'continent', 'total_cases'],
+        # category_orders={"location": df["location"].tolist().sort()}
+    )
 
-# Create Active Case Line Chart
-# @app.callback(
-#     Output("active-case", "figure"),
-#     [Input("dropdown", "value")]
-# )
-# def active_case(countries):
-#     mask = df.location.isin(countries)
-#     country_count = df.location.nunique()
-#     country_selected = df[mask].location.nunique()
-#
-#     fig = px.line(df[mask], x="date", y="total_cases",
-#                   color="continent", hover_name="location")
-#
-#     title = "Total Case in {} of {} countries".format(
-#         country_selected, country_count)
-#     fig.update_layout(title=title,
-#                       xaxis_title='Date', yaxis_title='Total Case')
-#     fig.update_traces(connectgaps=True, selector=dict(type='scatter'))
-#     return fig
+    title = "Total Case in {} of {} countries".format(
+        country_selected, country_count)
+    atv_fig.update_layout(
+        title=title,
+        xaxis_title='Date',
+        yaxis_title='Total Case'
+    )
+    atv_fig.update_traces(connectgaps=True)
+
+    return vcn_fig, atv_fig
 
 
 if __name__ == '__main__':
