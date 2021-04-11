77eb82bc24ea69d1b531b4933e377c8e2d313fc0
Kunanon Srisuntiroj <son9912@gmail.com>
2021-04-11 21:16:09 +0700

    Change dashboard into a data explorer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import time
 
 # App Initialize
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', 'https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -14,7 +15,7 @@ app = dash.Dash(
 )
 server = app.server
 
-app.title = "COVID-19 Data Visualize"
+app.title = "COVID-19 Data Explorer"
 app.config["suppress_callback_exceptions"] = True
 
 # Data Import
@@ -30,92 +31,150 @@ df = df.dropna(subset=['continent'])
 df = df.dropna(subset=['location'])
 
 # Calculate fields
-df['ppl_vaccinated'] = 100 * (df['people_vaccinated'] / df['population'])
+df['people_vaccinated_per_population'] = 100 * (df['people_vaccinated'] / df['population'])
+df['people_fully_vaccinated_per_population'] = 100 * (df['people_fully_vaccinated'] / df['population'])
 
-# Selection bar
+# Calculate fields
+a=int((time.time()/900-3)/2%24)
+curr_time = chr(128336+a//2+a%2*12)
+
+# Reused Components
+option_case_type = [
+    {'label': 'New confirmed cases', 'value': 'new_cases'},
+    {'label': 'New deaths attributed', 'value': 'new_deaths'},
+    {'label': 'Total confirmed cases', 'value': 'total_cases'},
+    {'label': 'Total deaths attributed', 'value': 'total_deaths'},
+    {'label': 'Vaccinated one dose', 'value': 'people_vaccinated'},
+    {'label': 'Vaccinated all doses', 'value': 'people_fully_vaccinated'},
+    {'label': 'Total confirmed cases per million people', 'value': 'total_cases_per_million'},
+    {'label': 'Total deaths per million people', 'value': 'total_deaths_per_million'},
+    {'label': 'Vaccinated one dose per population', 'value': 'people_vaccinated_per_population'},
+    {'label': 'Vaccinated all doses per population', 'value': 'people_fully_vaccinated_per_population'},
+]
+
+# Website Builder
 all_country = df.location.unique()
 app.layout = html.Div([
-    html.Div(children=[
-        html.H1('covid-vaccines'),
-    ]),
-    html.Div(children=[
-        html.H5('Change visible countries'),
+    html.Div([], className="one columns"),
+    html.Div([
+        html.Div(children=[
+            html.H1('COVID-19 Data Explorer'),
+        ]),
 
-        dcc.Dropdown(
-            id="dropdown",
-            options=[{"label": x, "value": x}
-                     for x in all_country],
-            value=['Thailand'],
-            multi=True
-        ),
-    ]),
-    html.Div(children=[
         html.Div(children=[
-            dcc.Graph(id="active-case"),
-        ], className="six columns"),
+
+            html.Div(children=[
+                html.H5('🌎 Countries'),
+                dcc.Dropdown(
+                    id="dropdown-country",
+                    options=[{"label": x, "value": x}
+                             for x in all_country],
+                    placeholder="Select a city",
+                    value=['Thailand'],
+                    multi=True
+                ),
+            ], className="three columns"),
+
+            html.Div(children=[
+                html.H5('📊 Case Type'),
+                dcc.Dropdown(
+                    id="dropdown-casetype",
+                    options=option_case_type,
+                    placeholder="Select a type",
+                    value='total_cases',
+                    multi=False,
+                    clearable=False,
+                    # disabled=True
+                ),
+            ], className="three columns"),
+
+            html.Div(children=[
+                html.H5(curr_time +' Time Range'),
+                dcc.Dropdown(
+                    id="dropdown-timerange",
+                    options=[
+                        {'label': 'All time', 'value': 'all'},
+                        {'label': '1 Week', 'value': '7d'},
+                        {'label': '2 Weeks', 'value': '14d'},
+                        {'label': '30 Days', 'value': '30d'},
+                        {'label': '90 Days', 'value': '90d'}
+                    ],
+                    placeholder="Select a range",
+                    value='all',
+                    multi=False,
+                    clearable=False,
+                    disabled=True
+                ),
+                # html.Button('Submit', id='submit-val', n_clicks=0),
+            ], className="three columns"),
+
+            html.Div(children=[
+                html.H5('🔢 Data Source'),
+                dcc.Dropdown(
+                    id="dropdown-datasource",
+                    options=[
+                        {'label': 'Default', 'value': 'default'},
+                    ],
+                    placeholder="Select a data source",
+                    value='default',
+                    multi=False,
+                    clearable=False
+                ),
+            ], className="three columns"),
+        ], className="row"),
+
+        # Chart
         html.Div(children=[
-            dcc.Graph(id="vaccinate-timeline"),
-        ], className="six columns"),
-    ], className="row"),
-    html.Div(children=[
-        html.P('Datasource from https://github.com/owid/covid-19-data/'),
-        html.P('Repository : https://github.com/sagelga/covid-vaccine'),
-    ]),
-])
+            dcc.Graph(id="active-case")
+        ]),
+
+        html.Div(children=[
+            html.P('Datasource from https://github.com/owid/covid-19-data/'),
+            html.P('Repository : https://github.com/sagelga/covid-vaccine'),
+        ]),
+    ], className="ten columns"),
+    html.Div([], className="one columns")
+], className="row")
 
 
 @app.callback(
-    Output("vaccinate-timeline", "figure"),
     Output("active-case", "figure"),
-    [Input("dropdown", "value")]
+    [Input("dropdown-country", "value"),
+     Input("dropdown-casetype", "value"),
+     Input("dropdown-timerange", "value"),
+     Input("dropdown-datasource", "value")]
 )
-def update_graph(countries):
+def update_graph(countries, case_type, time_range, data_source):
+    # Data Re-categorizing
+    # - Countries -
     mask = df.location.isin(countries)
+    # - Time Range -
+    time_range = "date"
 
     if len(countries) < 3:
         title_country = ' and '.join([str(i) for i in countries])
     else:
         title_country = str(len(countries)) + " selected countries"
 
-    # Create Vaccinated-Total Population Line Chart
-    vcn_fig = px.line(
-        df[mask],
-        x="date",
-        y="ppl_vaccinated",
-        title='Total Cases',
-        color="location",
-        hover_name="location",
-        hover_data=['date', 'continent', 'people_vaccinated', 'population', 'ppl_vaccinated']
-    )
-    title = "Population who received at least one vaccine dose in {}".format(
-        title_country)
-    vcn_fig.update_layout(
-        title=title,
-        xaxis_title='Date',
-        yaxis_title='Percent Vaccinated'
-    )
-    vcn_fig.update_traces(connectgaps=True)
-
     # Active Case Line Chart
-    atv_fig = px.line(
+    figure = px.line(
         df[mask],
-        x="date",
-        y="total_cases",
+        x=time_range,
+        y=case_type,
         color="location",
         hover_name="location",
-        hover_data=['date', 'continent', 'total_cases']
+        hover_data=['date', 'total_cases']
     )
 
-    title = "Total Case in {}".format(
-        title_country)
-    atv_fig.update_layout(
+    title = "{} in {}".format(case_type, title_country)
+    figure.update_layout(
         title=title,
-        xaxis_title='Date',
-        yaxis_title='Total Case'
+        xaxis_title="Date",
+        yaxis_title=str(case_type)
     )
-    atv_fig.update_traces(connectgaps=True)
+    figure.update_traces(connectgaps=True)
 
-    return vcn_fig, atv_fig
+    return figure
 
 
 if __name__ == '__main__':
