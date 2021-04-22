7f97b0a6e7eb0c06dbae931b406e667b887fcc00
Kunanon Srisuntiroj <son9912@gmail.com>
2021-04-22 20:45:36 +0700

    Add Chropleth chart + wireframing the website
    Strongly influenced by WHO Dashboard design https://covid19.who.int/

--- apps/world.py
+++ apps/world.py
@@ -33,11 +33,12 @@ df = df.dropna(subset=[
 # Data Transform
 df['date'] = pd.to_datetime(df['date'])
 df = df.sort_values(by=['date', 'location'])
-df['date_str'] = df['date'].apply(lambda x: str(x))
+df['date_str'] = df['date'].dt.date.apply(lambda x: str(x))
 
 # Calculate fields
 df['people_vaccinated_per_population'] = 100 * (df['people_vaccinated'] / df['population'])
 df['people_fully_vaccinated_per_population'] = 100 * (df['people_fully_vaccinated'] / df['population'])
+df['case_per_population'] = 100 * (df['total_cases'] / df['population'])
 
 # all_country = sorted(df["location"].unique())
 all_country = df["location"].unique()
@@ -45,24 +46,104 @@ all_country = df["location"].unique()
 a = int((time.time() / 900 - 3) / 2 % 24)
 curr_time = chr(128336 + a // 2 + a % 2 * 12)
 
-# Reused Components
-option_case_type = [
-    {'label': 'New confirmed cases', 'value': 'new_cases'}
-    , {'label': 'New deaths attributed', 'value': 'new_deaths'}
-    , {'label': 'Total confirmed cases', 'value': 'total_cases'}
-    , {'label': 'Total deaths attributed', 'value': 'total_deaths'}
-    , {'label': 'Vaccinated one dose', 'value': 'people_vaccinated'}
-    , {'label': 'Vaccinated all doses', 'value': 'people_fully_vaccinated'}
-    , {'label': 'Patient in Hospital', 'value': 'hosp_patients'}
-    , {'label': 'Patient in ICU', 'value': 'icu_patients'}
-    , {'label': 'Government Response Stringency Index', 'value': 'stringency_index'}
-    , {'label': 'Total confirmed cases per million people', 'value': 'total_cases_per_million'}
-    , {'label': 'Total deaths per million people', 'value': 'total_deaths_per_million'}
-    , {'label': 'Vaccinated one dose per population', 'value': 'people_vaccinated_per_population'}
-    , {'label': 'Vaccinated all doses per population', 'value': 'people_fully_vaccinated_per_population'}
-]
-
 # Website Builder
 layout = html.Div([
-    html.H2('Data'),
+    html.Div([
+        html.H2('World Trends'),
+
+        dcc.Graph(id='world-graph-choroplethview'),
+
+        html.Div([
+            html.Div(children=[
+                html.Label('{} Time Range'.format(curr_time)),
+                dcc.Dropdown(
+                    id="world-dropdown-choroplethview-timerange",
+                    options=[
+                        {'label': 'All time', 'value': 'all'}
+                        , {'label': '7 Days', 'value': '7'}
+                        , {'label': '14 Days', 'value': '14'}
+                        , {'label': '30 Days', 'value': '30'}
+                        , {'label': '90 Days', 'value': '90'}
+                        , {'label': '180 Days', 'value': '180'}
+                        , {'label': '365 Days', 'value': '365'}
+                    ]
+                    , placeholder="Select a range"
+                    , value='14'
+                    , multi=False
+                    , clearable=False
+                    , searchable=False
+                    , persistence=True
+                    , persistence_type='session'
+                ),
+            ], className="three columns"),
+        ], className='row'),
+    ]),
+
+    html.Br(),
+
+    html.H2('Global Situation'),
+    html.Div([
+        html.Div([dcc.Graph()], className='six columns'),
+        html.Div([dcc.Graph()], className='six columns'),
+    ], className='row'),
+
+    html.Br(),
+
+    html.H3('Situation by Region'),
+    dcc.Graph(),
+    html.Div([
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+    ]),
+
+    html.Br(),
+
+    html.H3('Situation by Country'),
+    html.Div([
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+        html.Div([dcc.Graph()], className='three columns'),
+    ]),
 ])
+
+
+@app.callback(
+    Output("world-graph-choroplethview", "figure"),
+    Input("world-dropdown-choroplethview-timerange", "value")
+)
+def sth(time_range):
+    # - Time Range -
+    time_max = datetime.today()
+    time_list = []
+
+    if time_range == 'all':
+        time_range = time_max = timedelta(df['date'].min())
+
+    for _ in range(1, int(time_range) + 1):
+        time_check = time_max - timedelta(days=_)
+        # if time_check in df['date']:
+        time_check = time_check.strftime("%Y-%m-%d")
+        time_list.append(time_check)
+
+    # - Apply mask -
+    mask = df['date'].isin(time_list)
+
+    return px.choropleth(df[mask]
+                         , locations="iso_code"
+                         , color='case_per_population'
+                         , hover_name="location"
+                         , animation_frame="date_str"
+                         , color_continuous_scale=px.colors.sequential.Viridis
+                         , projection='kavrayskiy7'
+                         )
