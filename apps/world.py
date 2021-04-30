5542c1f18a99a2ea98b14357945abbc64ccbcf91
Kunanon Srisuntiroj <son9912@gmail.com>
2021-04-30 11:15:12 +0700

    Move data compute to one component

--- apps/world.py
+++ apps/world.py
@@ -10,35 +10,12 @@ from datetime import datetime, timedelta
 import time
 
 from app import app
+from component import owid
 
-# Data Import
-url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
-df = pd.read_csv(url)
-
-# Data selection
-df = df[['iso_code', 'continent', 'location', 'date', 'total_cases', 'new_cases', 'total_deaths', 'new_deaths',
-         'total_cases_per_million', 'new_cases_per_million', 'total_deaths_per_million', 'new_deaths_per_million',
-         'icu_patients', 'icu_patients_per_million', 'hosp_patients', 'hosp_patients_per_million', 'new_tests',
-         'total_tests', 'total_tests_per_thousand', 'new_tests_per_thousand', 'positive_rate', 'tests_per_case',
-         'tests_units', 'total_vaccinations', 'people_vaccinated', 'people_fully_vaccinated', 'new_vaccinations',
-         'stringency_index', 'population']]
-
-# NA Data Drop
-df = df.dropna(subset=[
-    'date'
-    , 'continent'
-    , 'location'
-])
+df = owid.df
 
 # Data Transform
-df['date'] = pd.to_datetime(df['date'])
-df = df.sort_values(by=['date', 'location'])
 df['date_str'] = df['date'].dt.date.apply(lambda x: str(x))
-
-# Calculate fields
-df['people_vaccinated_per_population'] = 100 * (df['people_vaccinated'] / df['population'])
-df['people_fully_vaccinated_per_population'] = 100 * (df['people_fully_vaccinated'] / df['population'])
-df['case_per_population'] = 100 * (df['total_cases'] / df['population'])
 
 # all_country = sorted(df["location"].unique())
 all_country = df["location"].unique()
